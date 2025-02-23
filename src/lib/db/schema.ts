716113fe84a49e7e6b9a6eefedf236593d1c89af
Tyler Henderson <tyler.henderson@layton.local>
2025-02-22 17:39:25 -0800

    Fixed error in schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, text, uuid, integer } from "drizzle-orm/pg-core"
+import { pgTable, text, uuid } from "drizzle-orm/pg-core"
 
 export const profileTable = pgTable("profile", {
-    id: integer("id").primaryKey(),
+    id: uuid("id").primaryKey().notNull(),
     first_name: text("first_name").notNull(),
     last_name: text("last_name").notNull(),
     email: text("email").notNull(),
